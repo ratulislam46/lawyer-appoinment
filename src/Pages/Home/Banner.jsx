7584fc9dc70d9eafbd45e32cde67c51ff5dc7d82
ratulislam46 <arfanratul46@gmail.com>
2025-04-21 22:08:35 +0600

    lawyers booking section complete

--- src/Pages/Home/Banner.jsx
+++ src/Pages/Home/Banner.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Banner = () => {
     return (
         <div
-            className="hero h-[600px] mt-1"
+            className="hero h-[400px] md:h-[500px] lg:h-[600px] mt-1"
             style={{
                 backgroundImage:
                     "url(https://i.postimg.cc/14xmHYJy/banner.png)",
@@ -11,13 +11,11 @@ const Banner = () => {
         >
             <div className="hero-overlay"></div>
             <div className="hero-content text-neutral-content text-center">
-                <div className="max-w-md">
-                    <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-                    <p className="mb-5">
-                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                        quasi. In deleniti eaque aut repudiandae et a id nisi.
+                <div className="max-w-5xl">
+                    <h1 className="mb-5 text-5xl font-bold">It avoids subjective claims or exaggeration that might raise red flags legally</h1>
+                    <p className="mb-5 text-lg text-gray-400">
+                    Our platform connects you with verified, experienced doctors across various specialties — all at your convenience. Whether it's a routine checkup or urgent consultation, book appointments in minutes and receive quality care you can trust.
                     </p>
-                    <button className="btn btn-primary">Get Started</button>
                 </div>
             </div>
         </div>
--- src/Pages/Lawyers/ViewDetails.jsx
+++ src/Pages/Lawyers/ViewDetails.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link, useLoaderData, useParams } from 'react-router';
 import { addBook } from '../../Utility';
+import { BiError } from "react-icons/bi";
 
 
 const ViewDetails = () => {
@@ -30,32 +31,37 @@ const ViewDetails = () => {
             </div>
 
             {/* details section  */}
-            <div className='flex space-x-5 border border-gray-300 rounded-xl p-5 my-5'>
+            <div className='flex space-x-5 border border-gray-300 rounded-xl p-5 my-5 items-center'>
                 <div>
                     <img src={image} alt="Lawyer photo" />
                 </div>
-                <div>
-                    <p>{experience}</p>
-                    <h1>{name}</h1>
-                    <p ><span className='mr-4'>{speciality}</span> {licenseNumber}</p>
+                <div className='space-y-4'>
+                    <p className='text-blue-600 px-4 py-1 bg-blue-100 text-center rounded-lg inline-block'>{experience}</p>
+                    <h1 className='text-3xl font-semibold'>{name}</h1>
+                    <p className='text-gray-500 text-xl'><span className='mr-4'>{speciality}</span> {licenseNumber}</p>
                     <div className='flex gap-4'>
-                        <p>Availability </p>
+                        <p className='font-semibold text-xl'>Availability : </p>
                         {
-                            availability.map((available, index) => <p key={index}>{available}</p>)
+                            availability.map((available, index) => <p className='text-yellow-400 text-xl' key={index}>{available}</p>)
                         }
                     </div>
-                    <p>Consultation Fee: <span>Taka: {fee}</span></p>
+                    <p className='font-semibold text-xl'>Consultation Fee: <span className='text-green-600'>Taka {fee}</span></p>
                 </div>
+            </div>
+
+            {/* title  */}
+            <div className='mb-4'>
+                <h1 className='text-center text-4xl font-bold'>Book an Appointment</h1>
             </div>
 
             {/* Book appointment button  */}
             <div className='border border-gray-300 p-5 mb-5 rounded-xl'>
-                <h1 className='text-center text-4xl'>Book an Appointment</h1>
-                <p className='flex justify-between'><span>Availability</span> <span>Lawyer Available Today</span></p>
-                <p>Due to high patient volume, we are currently accepting appointments for today only. We appreciate your understanding and cooperation.</p>
+
+                <p className='flex justify-between border-b border-gray-400 pb-2'><span className='font-bold'>Availability</span> <span className='font-semibold text-[#ef476f]'>Lawyer Available Today</span></p>
+                <p className='text-yellow-400 py-4 flex items-center gap-1'> <span><BiError size={20} /></span> Due to high patient volume, we are currently accepting appointments for today only. We appreciate your understanding and cooperation.</p>
 
                 <Link onClick={handleButton}>
-                <button className='btn w-full'>Book Appointment Now</button>
+                    <button className='btn w-full bg-[#ef476f] hover:bg-green-500 rounded-3xl text-white'>Book Appointment Now</button>
                 </Link>
 
             </div>
